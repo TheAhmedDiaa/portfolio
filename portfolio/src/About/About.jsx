c130f4c44ed41aed4ce5833a8cf687a8cb8b2753
AhmedDiaa69 <diaaa625@gmail.com>
2025-08-29 02:43:29 +0300

    Add About section component

---
+++ portfolio/src/About/About.jsx
@@ -0,0 +1,43 @@
+export default function About() {
+  return (
+    <section id="About" data-aos="fade-up">
+      <h2 className="text-3xl font-bold text-center m-3 ">
+        About me{" "}
+        <i
+          className="bx  bx-user-id-card align-bottom"
+          style={{ color: "#8000ff" }}
+        ></i>{" "}
+      </h2>
+      <p className="max-w-252 text-center mx-auto my-0 p-4 text-lg">
+        Hi 👋, 'm Ahmed Diaa, a{" "}
+        <a
+          className="text-(--accent) hover:text-(--accent-hover)"
+          href="https://react.dev/"
+          target="_blank"
+        >
+          React
+        </a>{" "}
+        web developer with a passion for creating dynamic and engaging web
+        applications. I'm currently a second year studen at Faculty of computer
+        & information science (
+        <a
+          className="text-(--accent) hover:text-(--accent-hover)"
+          href="https://csifac.mans.edu.eg/"
+          target="_blank"
+        >
+          FCIS
+        </a>
+        ) at{" "}
+        <a
+          className="text-(--accent) hover:text-(--accent-hover)"
+          href="https://www.mans.edu.eg/"
+          target="_blank"
+        >
+          Mansoura university
+        </a>
+        . I focus on building responsive and accessible web applications that
+        prioritize SEO, UI/UX, and performance.
+      </p>
+    </section>
+  );
+}
